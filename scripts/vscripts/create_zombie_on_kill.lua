1082fa94eb4a7ad6e43d5b16eb2df145f0aab118
Joseph <mate.joseph@gmail.com>
2016-03-27 20:30:37 -0400

    initial code and data added for create zombie on kill
    I am uncertain why it's not working. I added print messages and I'm not
    seeing them.

---
+++ scripts/vscripts/create_zombie_on_kill.lua
@@ -0,0 +1,23 @@
+
+
+--------------------------------------------------------------------------------
+
+-- used sample:
+-- data driven: https://github.com/Pizzalol/SpellLibrary/blob/0ebc18d0f34b109b33887848709041cfcc5e05fb/game/scripts/npc/abilities/alchemist/goblins_greed_datadriven.txt 
+-- lua:         https://github.com/Pizzalol/SpellLibrary/blob/0ebc18d0f34b109b33887848709041cfcc5e05fb/game/scripts/vscripts/heroes/hero_alchemist/goblins_greed.lua
+function CreateZombieOnKill(event)
+	-- Variables
+	local killer = event.caster
+	local unitKilled = event.unit
+	local player = PlayerResource:GetPlayer( caster:GetPlayerID() )
+	
+	Msg("CreateZombieOnKill")
+	PrintLinkedConsoleMessage("info", "CreateZombieOnKill")
+	if unitKilled:GetUnitName() == "npc_civilian" then
+		Msg("Was npc_civlian, making npc_zombie")
+		PrintLinkedConsoleMessage("info", "Was npc_civlian, making npc_zombie")
+		--  handle CreateUnitByName(string string_1, Vector Vector_2, bool bool_3, handle handle_4, handle handle_5, int int_6)
+		-- Creates a DOTA unit by its dota_npc_units.txt name ( szUnitName, vLocation, bFindClearSpace, hNPCOwner, hUnitOwner, iTeamNumber ) 
+		local zombie = CreateUnitByName("npc_zombie", target:GetAbsOrigin(), true, nil, nil, killer:GetTeam())
+	end
+end
