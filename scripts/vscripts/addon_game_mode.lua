691c77eed84818afc0b09932c68a724fa42a89cf
Joseph <mate.joseph@gmail.com>
2016-04-05 01:45:13 -0400

    things are moving around randomly but not attacking
    TODO:
    - civilians aren't running away from zombies
    - zombies aren't attack targeting
    - new zombies created from killing do not have timers

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -1,4 +1,5 @@
 
+require('libraries/timers')
 
 if GameMode == nil then
 	GameMode = class({})
@@ -55,49 +56,131 @@ function GameMode:OnPlayerChat(keys)
 --[[
 	-- how to check for player name
     if tostring(PlayerResource:GetPlayerName(hero:GetPlayerOwnerID())) == "fokita" then
-      if text == "dev_mode" then
-        GameMode:CC_dev()
-      end
-    
-
-    if dev_mode == true then
-      if text == "att_r" then
-        GameMode:att_r()
-      end
-
-      if text == "g2" then
-        GameMode:CC_g2()
-      end
-      
-      if text == "e9" then
-        GameMode:CC_e9()
-      end
-      
-      if text == "e2" then
-        GameMode:CC_e2()
-      end
-      
-      if text == "g1" then
-        GameMode:CC_g1()
-      end
-      
-      if text == "att_d" then
-        GameMode:att_d()
-      end
-
-      if text == "LUS" then
-        GameMode:LightUpShowcase()
-      end
-
-      if text == "l2" then
-        for i=1,cpListLength do
-          GameMode:CC_LightDown(i)
-        end
-      end
 ]]
+
 end
 
+function moveNpc(npc, newPosn)
+  local newOrder = {
+    UnitIndex = npc:entindex(), 
+    OrderType =  DOTA_UNIT_ORDER_MOVE_TO_POSITION,
+    TargetIndex = nil, --Optional.  Only used when targeting units
+    AbilityIndex = 0, --Optional.  Only used when casting abilities
+    Position = newPosn, --Optional.  Only used when targeting the ground
+    Queue = 0 --Optional.  Used for queueing up abilities
+  }
+
+  ExecuteOrderFromTable(newOrder)
+end
+
+function moveRandomAngle(npc)
+  local angle = RandomFloat(0,2*math.pi)
+  local movementX = 1000*math.cos(angle)
+  local movementY = 1000*math.sin(angle)
+  local movementVector = Vector(movementX,movementY,0)
+  local newPosn = npc:GetAbsOrigin() + movementVector
+
+  -- move randomly
+  moveNpc(npc,newPosn)
+end
+
+function findNearbyUnits(npc)
+  local units = FindUnitsInRadius(
+        npc:GetTeamNumber(),         --iTeamNumber, 
+        npc:GetAbsOrigin(),          --vPosition, 
+        nil,                         --hCacheUnit, 
+        800,                         --flRadius, 
+        DOTA_UNIT_TARGET_TEAM_ENEMY, --iTeamFilter, 
+        DOTA_UNIT_TARGET_NONE,       --iTypeFilter, 
+        DOTA_UNIT_TARGET_FLAG_NONE,  --iFlagFilter, 
+        FIND_CLOSEST,                --iOrder,
+        false                        --bCanGrowCache
+      )
+  return units
+end
+
+function zombieMoveFunction(zombie)
+  return function()
+    if zombie:IsAlive() then
+      local units = findNearbyUnits(zombie)
+
+      local closestEnemy = nil;
+      local enemyNearby = false;
+      for key,unit in pairs(units) do
+        if not enemyNearby  then
+          enemyNearby = true
+          closestEnemy = unit
+        end
+      end
+
+      if enemyNearby then
+        -- move towards enemy and attack
+        local newOrder = {
+          UnitIndex = zombie:entindex(), 
+          OrderType =  DOTA_UNIT_ORDER_ATTACK_TARGET,
+          TargetIndex = closestEnemy:entindex(), --Optional.  Only used when targeting units
+          AbilityIndex = 0, --Optional.  Only used when casting abilities
+          Position = nil, --Optional.  Only used when targeting the ground
+          Queue = 0 --Optional.  Used for queueing up abilities
+        }
+ 
+        ExecuteOrderFromTable(newOrder)
+      else
+        moveRandomAngle(zombie)
+      end
+
+      Timers:CreateTimer( 1.0 , zombieMoveFunction(zombie) )
+    end
+  end
+end
+
+
+function civilianMoveFunction(civilian)
+  return function()
+    if civilian:IsAlive() then
+      local units = findNearbyUnits(civilian)
+
+      local accumulatedVector = Vector(0,0,0);
+      local enemyNearby = false;
+      for key,unit in pairs(units) do
+        enemyNearby = true
+        accumulatedVector = accumulatedVector + (civilian:GetAbsOrigin() - unit:GetAbsOrigin())
+      end
+
+      -- move away from the enemies
+      -- computed by taking the sum of the vectors pointing to the civilian
+      if enemyNearby then
+        local accumVectorLen = accumulatedVector.Length()
+        if accumVectorLen > 0 then 
+          newPosn = civilian:GetAbsOrigin() 
+            + Vector(
+              1000*accumulatedVector.x/accumVectorLen,
+              1000*accumulatedVector.y/accumVectorLen,
+              0)
+          moveNpc(npc,newPosn)
+        end
+      else
+        moveRandomAngle(civilian)
+      end
+
+      Timers:CreateTimer( 1.0 , civilianMoveFunction(civilian) )
+    end
+  end
+end
+
+function addZombie(x,y)
+  local zombie = CreateUnitByName("npc_zombie", Vector(x,y,0), true, nil, nil, DOTA_TEAM_BADGUYS)
+  Timers:CreateTimer( 1.0 , zombieMoveFunction(zombie) )
+  return zombie
+end
+
+function addCivilian(x,y)
+  local civ = CreateUnitByName("npc_civilian", Vector(x,y,0), true, nil, nil, DOTA_TEAM_NEUTRALS)
+  Timers:CreateTimer( 1.0 , civilianMoveFunction(civ) )
+  return civ
+end 
+
 function Level1()
-	local civ = CreateUnitByName("npc_civilian", Vector(0,-200,0), true, nil, nil, DOTA_TEAM_NEUTRALS)
-	local zombie = CreateUnitByName("npc_zombie", Vector(0,200,0), true, nil, nil, DOTA_TEAM_BADGUYS)
+	local civ = addCivilian(0,-200)
+	local zombie = addZombie(0,200)
 end
